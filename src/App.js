52b7141e08d487fc1b41f957ce556f43ec7dcc70
qasem-moh <2171246@s.aou.edu.jo>
2021-06-27 21:28:34 +0300

    lab06

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import City from './components/city'
-import logo from './logo.svg';
+// import NavBar from './components/nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* <NavBar /> */}
       <City />
     </div>
   );
